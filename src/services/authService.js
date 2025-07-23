14c6203a55ebf883924f2bb72e86ede45eb05742
Azanou <azanoupresciencedutemps@gmail.com>
2025-07-23 20:35:17 +0100

    modifs

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // --- Configuration ---
-const KEYCLOAK_URL = 'http://localhost:8180/realms/GasStockRealm/protocol/openid-connect/token';
-const API_URL = 'http://localhost:8080/api/v1';
+const KEYCLOAK_URL = 'https://gasflow-keycloak.onrender.com/realms/GasStockRealm/protocol/openid-connect/token';
+const API_URL = 'https://gas-stock-management-t57r.onrender.com/api/v1';
 
 // --- SUPPRIMÉ ---
 // Le secret ne doit JAMAIS être dans le code du frontend.
